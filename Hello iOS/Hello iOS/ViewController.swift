4a018892625b141b86e59f033d1dbf3b16383c9b
Rico Wang <ricowang@uw.edu>
2018-09-30 18:32:36 -0700

    built and finished

---
+++ Hello iOS/Hello iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello iOS
+//
+//  Created by Rico Wang on 9/30/18.
+//  Copyright © 2018 Rico Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
